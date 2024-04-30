096e70e5617411173a0c0c0ad6f62c3a80e27bbf
Tercette <matheus01tercette@gmail.com>
2024-04-30 16:49:13 -0300

    Atualização do código e fim da primeira parte de texto

--- TerVisual/Interpretador_TerVisual.py
+++ TerVisual/Interpretador_TerVisual.py
@@ -2,52 +2,83 @@ import flet as ft
 
 
 def main(pagina):
-    arquivo = open('C:\\Users\\mathe\\OneDrive\\Área de Trabalho\\Criações\\TerceQL\\TerVisual\\text.TV', 'r')
-    codigo_bruto = arquivo.read()
-    codigo = codigo_bruto.split("; ")
-    for interpretacao in codigo:
-        comando = interpretacao.split(": ")
-        if comando[0] == "escreva":
-            #caso a pessoa queira usar o terminal
-            print(comando[1])
+    with open('C:\\Users\\mathe\\OneDrive\\Área de Trabalho\\Criações\\TerceQL\\TerVisual\\text.TV', 'r') as arquivo:
+        linhas = arquivo.readlines()
 
-        elif comando[0] == "texto":
-            #formas da pessoa colocar o texto
+    for linha in linhas:
+        codigo = linha.split("; ")
+        for interpretacao in codigo:
+            comando = interpretacao.split(": ")
+            if comando[0] == "escreva":
+                #caso a pessoa queira usar o terminal
+                print(comando[1])
 
-            #organização de subcomandos
-            subcomandos = comando[1].split(", ")
-            print(subcomandos)
+            elif comando[0] == "texto":
+                #formas da pessoa colocar o texto
 
-            if int(subcomandos[0]) == 1:
-                #forma padrão de escrever o texto
-                texto = ft.Text(subcomandos[1])
-                pagina.add(texto)
+                #organização de subcomandos
+                subcomandos = comando[1].split(", ")
+                print(subcomandos)
 
-            elif int(subcomandos[0]) == 2:
-                #organização das ordens de cor
-                cor = subcomandos[2].split(".")
-
-                print(subcomandos[2].isdigit())
-
-                #para identificar qual comando o segundo comando
-                if subcomandos[2].isdigit():
-                    tipo = type(int(subcomandos[2]))
-                else:
-                    tipo = type(subcomandos[2])
-
-                if tipo == int:
-                    #forma com tamanho de escrever o texto
-                    print(subcomandos)
-                    texto = ft.Text(subcomandos[1], size=int(subcomandos[2]))
+                if int(subcomandos[0]) == 1:
+                    #forma padrão de escrever o texto
+                    texto = ft.Text(subcomandos[1])
                     pagina.add(texto)
-                        
-                elif cor[0] == "cor":
-                        #forma com cor de escrever o texto
-                        texto = ft.Text(subcomandos[1], color = cor[1])
+
+                elif int(subcomandos[0]) == 2:
+                    #organização das ordens de cor
+                    cor = subcomandos[2].split(".")
+
+                    #organização das ordens italicas
+                    italico = subcomandos[2].split(".")
+
+                    #organização das ordens grifar
+                    grifar = subcomandos[2].split(".")
+
+                    #organização das ordens fonte
+                    familia_fonte = subcomandos[2].split(".")
+
+                    print(subcomandos[2].isdigit())
+
+                    #para identificar qual comando o segundo comando
+                    if subcomandos[2].isdigit():
+                        tipo = type(int(subcomandos[2]))
+                    else:
+                        tipo = type(subcomandos[2])
+
+                    if tipo == int:
+                        #forma com tamanho de escrever o texto
+                        print(subcomandos)
+                        texto = ft.Text(subcomandos[1], size=int(subcomandos[2]))
                         pagina.add(texto)
+                            
+                    elif cor[0] == "cor":
+                            #forma com cor de escrever o texto
+                            texto = ft.Text(subcomandos[1], color = cor[1])
+                            pagina.add(texto)
+                    
+                    elif italico[0] == "italico":
+                            if italico[1] == "verdadeiro":
+                                #tranforma o texto em italico
+                                texto = ft.Text(subcomandos[1], italic=True)
+                                pagina.add(texto)
+
+                            elif italico[2] == "falso":
+                                 #não tranforma o texto em italico
+                                 texto = ft.Text(subcomandos[1], italic=False)
+                                 pagina.add(texto)
+                    
+                    elif grifar[0] == "grifar":
+                        #forma com grifar de escrever o texto
+                        cor_grifar = grifar[1].upper()
+                        texto = ft.Text(subcomandos[1], bgcolor=cor_grifar)
+                        pagina.add(texto)
+                    
+                    elif familia_fonte[0] == "familia_fonte":
+                         pagina.theme = ft.Theme(font_family=familia_fonte[1])
+                     
+                
+
+                            
 
 ft.app(main, view=ft.WEB_BROWSER)
-        
-
-    
-        
